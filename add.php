04c1600cdf6bc2b0550b4e8cc2a2abec81dfbfd6
matlegdal <matlegdal@hotmail.com>
2017-11-22 10:48:40 -0500

    add server implementation of add.php

--- add.php
+++ add.php
@@ -8,6 +8,35 @@ if (!isset($_SESSION['user_id'])) {
 	return;
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if (strlen($_POST['first_name'])>0 && strlen($_POST['last_name'])>0 && strlen($_POST['email'])>0 && strlen($_POST['headline'])>0 && strlen($_POST['summary'])>0) {
+		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$query = $pdo->prepare("INSERT INTO profiles (user_id, first_name, last_name, email, headline, summary) VALUES (:user_id, :first_name, :last_name, :email, :headline, :summary)");
+			$query->execute(array(
+				':user_id' => $_SESSION['user_id'],
+				':first_name' => $_POST['first_name'],
+				':last_name' => $_POST['last_name'],
+				':email' => $_POST['email'],
+				':headline' => $_POST['headline'],
+				':summary' => $_POST['summary']
+			));
+
+			$_SESSION['success'] = 'Record added';
+			header('Location: index.php');
+			return;
+		} else {
+			$_SESSION['error'] = 'Please enter a valid email address.';
+			header('Location: add.php');
+			return;
+		}
+	} else {
+		$_SESSION['error'] = 'All fields are required';
+		header('Location: add.php');
+		return;
+	}
+}
+
+require 'flash.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,30 +46,29 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 <body>
 	<h1>Add a new profile</h1>
+	<?=$flash?>
 	<form action="add.php" method="POST">
 		<p>Contact information:</p>
 		<p>
-			<input type="text" name="first_name" placeholder="First name" required>
-			<input type="text" name="last_name" placeholder="Last name" required>
-			<input type="email" name="email" placeholder="Email" required>
+			<input type="text" name="first_name" placeholder="First name" >
+			<input type="text" name="last_name" placeholder="Last name" >
+			<input type="email" name="email" placeholder="Email" >
 		</p>
 		<p>
 			Headline:
 		</p>
 		<p>
-			<input type="text" name="headline" placeholder="Enter your headline" required>
+			<input type="text" name="headline" placeholder="Enter your headline" >
 		</p>
 		<p>
 			Summary:
 		</p>
 		<p>
-			<textarea name="summary" placeholder="Enter a brief summary" required></textarea>
+			<textarea name="summary" placeholder="Enter a brief summary" ></textarea>
 		</p>
-		<p>
-			<input type="submit" value="Add profile">
-			<input type="reset" value="Reset">
-			<button href="index.php">Retour à l'index</button>
-		</p>
+		<input type="submit" value="Add profile">
+		<input type="reset" value="Reset">		
 	</form>
+	<a href="index.php">Retour à l'index</a>
 </body>
 </html>
