8444d6e53004ac90cb737b5e2face8e6e62d6bbc
matlegdal <matlegdal@hotmail.com>
2017-11-22 00:09:34 -0500

    add server side login implementation

--- login.php
+++ login.php
@@ -8,7 +8,24 @@ if (isset($_SESSION['user'])) {
 	return;
 }
 
+if (isset($_POST['email']) && isset($_POST['password'])) {
+	$salt = 'XyZzy12*_';
+	$check = hash('md5', $salt.$_POST['password']);
+	$query = $pdo->prepare("SELECT user_id, name FROM users WHERE email = :email AND password = :password");
+	$query->execute(array(':email' => $_POST['email'], ':password' => $check));
+	$user = $query->fetch(PDO::FETCH_ASSOC);
 
+	if ($user !== false) {
+		$_SESSION['name'] = $user['name'];
+		$_SESSION['user'] = $user['user_id'];
+		header('Location: index.php');
+		return;
+	} else {
+		$_SESSION['error'] = "Your email and password are not valid.";
+		header('Location: login.php');
+		return;
+	}
+}
 
 require 'flash.php';
 ?>
