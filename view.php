d4a877427768899c13043aa3aaf8ddfabcb9b6ac
matlegdal <matlegdal@hotmail.com>
2017-11-22 17:31:40 -0500

    refactor the design of the view page

--- view.php
+++ view.php
@@ -33,33 +33,36 @@ if ($profile === false) {
 </head>
 <body>
 	<?php require 'partials/navbar.php'; ?>
-	<div class="container">
-		<h1>Profile - <?= htmlentities($profile['first_name']).' '.htmlentities($profile['last_name']) ?></h1>
+	<div class="container" style="margin: 2em">
 		<?=flash()?>
-		<p>First name: <?= htmlentities($profile['first_name'])?></p>
-		<p>Last name: <?= htmlentities($profile['last_name'])?></p>
-		<p>Email: <?= htmlentities($profile['email'])?></p>
-		<p>Headline: <?= htmlentities($profile['headline'])?></p>
-		<p>Summary: <?= htmlentities($profile['summary'])?></p>
-		<h4>Positions:</h4>
-		
-		<?php
-			if (count($positions) == 0) {
-				echo "<p>No position yet</p>";
-			} else {
-				echo "<ul>";
-				foreach ($positions as $position) {
-					echo "<li>";
-					echo $position['year'];
-					echo " - ";
-					echo $position['description'];
-					echo "</li>";
-				}
-				echo "</ul>";
-			}
-		?>
-		</ul>
-		<p><a href="index.php">Done</a></p>
+		<div class="card">
+			<h1 class="card-header">Profile - <?= htmlentities($profile['first_name']).' '.htmlentities($profile['last_name']) ?></h1>
+			<div class="card-body">
+				<p class="card-text">First name: <?= htmlentities($profile['first_name'])?></p>
+				<p class="card-text">Last name: <?= htmlentities($profile['last_name'])?></p>
+				<p class="card-text">Email: <?= htmlentities($profile['email'])?></p>
+				<p class="card-text">Headline: <?= htmlentities($profile['headline'])?></p>
+				<p class="card-text">Summary: <?= htmlentities($profile['summary'])?></p>
+				<h4>Positions:</h4>
+				
+				<?php
+					if (count($positions) == 0) {
+						echo "<p>No position yet</p>";
+					} else {
+						echo "<ul>";
+						foreach ($positions as $position) {
+							echo "<li>";
+							echo $position['year'];
+							echo " - ";
+							echo $position['description'];
+							echo "</li>";
+						}
+						echo "</ul>";
+					}
+				?>
+			</div>
+		</div>
+		<a class="btn btn-primary" href="index.php" style="margin: 1em">Done</a>
 	</div>
 </body>
 </html>
