5315f6da156a29968d67ff6f2e2e233066bc569b
matlegdal <matlegdal@hotmail.com>
2017-11-22 16:37:37 -0500

    add list of positions in view profile

--- view.php
+++ view.php
@@ -13,6 +13,10 @@ $query = $pdo->prepare("SELECT * FROM profiles WHERE profile_id = :profile_id");
 $query->execute(array(':profile_id' => $_GET['profile_id']));
 $profile = $query->fetch(PDO::FETCH_ASSOC);
 
+$query = $pdo->prepare("SELECT * FROM positions WHERE profile_id = :profile_id");
+$query->execute(array(':profile_id' => $_GET['profile_id']));
+$positions = $query->fetchAll(PDO::FETCH_ASSOC);
+
 if ($profile === false) {
 	$_SESSION['error'] = 'The profile you requested is not found.';
 	header('Location: index.php');
@@ -36,6 +40,24 @@ if ($profile === false) {
 		<p>Email: <?= htmlentities($profile['email'])?></p>
 		<p>Headline: <?= htmlentities($profile['headline'])?></p>
 		<p>Summary: <?= htmlentities($profile['summary'])?></p>
+		<h4>Positions:</h4>
+		
+		<?php
+			if (count($positions) == 0) {
+				echo "<p>No position yet</p>";
+			} else {
+				echo "<ul>";
+				foreach ($positions as $position) {
+					echo "<li>";
+					echo $position['year'];
+					echo " - ";
+					echo $position['description'];
+					echo "</li>";
+				}
+				echo "</ul>";
+			}
+		?>
+		</ul>
 		<p><a href="index.php">Done</a></p>
 	</div>
 </body>
